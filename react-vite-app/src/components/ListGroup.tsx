6a8c5c7185e460d54c1b4f6839f33e7b093a11ad
Edward Long <edwardlong@Edwards-MacBook-Pro.local>
2025-12-15 15:39:01 +0000

    Added event handler for when list items are clicked

--- react-vite-app/src/components/ListGroup.tsx
+++ react-vite-app/src/components/ListGroup.tsx
@@ -1,10 +1,17 @@
 import { Fragment } from "react/jsx-runtime";
+import { MouseEvent } from "react";
 
 function ListGroup() {
+  // All arrays have an index that we can access in the .map() method
   let items = ["New York", "San Francisco", `Tokyo`, `London`, `Paris`];
-  items = [];
+  //   items = [];
 
   const message = items.length === 0 && <p>No item found</p>;
+
+  // Event handler
+  // To handle event logic we should use a separate function within the component
+  // by adding : MouseEvent we have used type annotiation, and now we have autocompletion when we get props of event
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     // React Fragment lets you have many html tags in one return statement
@@ -17,8 +24,11 @@ function ListGroup() {
         {/* The items.map(...) expression needs to be wrapped in braces*/}
         {/* A key for each item is required so React knows which element is which for dynamic activites */}
         {/* Often when bring items from an API each item will have an id property item.id */}
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          // In react each element has a property called onClick, in this example we use an arrow function
+          <li key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
