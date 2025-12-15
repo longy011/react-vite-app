4e521103893bf0dac40169fd071b2c8b5cb260b8
Edward Long <edwardlong157@gmail.com>
2025-12-15 16:51:22 +0000

    Added alert component with children prop

--- react-vite-app/src/App.tsx
+++ react-vite-app/src/App.tsx
@@ -1,3 +1,4 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
 
   return (
     <div>
+      <Alert>
+        Hello <span> World</span>
+      </Alert>
       <ListGroup
         items={items}
         heading="Cities"
